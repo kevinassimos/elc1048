23df43608608116a6e6cd46bf652ddc913d86836
kevinassimos <kevin.assimos@acad.ufsm.br>
2023-05-24 14:46:47 -0300

    Update main.c
    Modificacao para adicionar uma nova tarefa.

--- MULTITAREFAS/MULTITAREFAS_R21/src/main.c
+++ MULTITAREFAS/MULTITAREFAS_R21/src/main.c
@@ -17,8 +17,8 @@
  * \par Conteudo
  *
  * -# Inclui funcoes do sistema multitarefas (atraves de multitarefas.h)
- * -# InicializaÁ„o do processador e do sistema multitarefas
- * -# CriaÁ„o de tarefas de demonstraÁ„o
+ * -# Inicialização do processador e do sistema multitarefas
+ * -# Criação de tarefas de demonstração
  *
  */
 
@@ -40,6 +40,7 @@ void tarefa_5(void);
 void tarefa_6(void);
 void tarefa_7(void);
 void tarefa_8(void);
+void tarefa_9(void);
 
 /*
  * Configuracao dos tamanhos das pilhas
@@ -52,6 +53,7 @@ void tarefa_8(void);
 #define TAM_PILHA_6			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_7			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_8			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_9			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
 
 /*
@@ -65,6 +67,7 @@ uint32_t PILHA_TAREFA_5[TAM_PILHA_5];
 uint32_t PILHA_TAREFA_6[TAM_PILHA_6];
 uint32_t PILHA_TAREFA_7[TAM_PILHA_7];
 uint32_t PILHA_TAREFA_8[TAM_PILHA_8];
+uint32_t PILHA_TAREFA_9[TAM_PILHA_9];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
 
 /*
@@ -80,6 +83,8 @@ int main(void)
 	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 1);
 	
 	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 2);
+	
+	CriaTarefa(tarefa_9, "Tarefa 9", PILHA_TAREFA_9, TAM_PILHA_9, 3);
 	
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
@@ -104,7 +109,7 @@ void tarefa_1(void)
 	{
 		a++;
 		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE); /* Liga LED. */
-		TarefaContinua(2);
+		TarefaContinua(3);
 	
 	}
 }
@@ -119,6 +124,7 @@ void tarefa_2(void)
 		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE); 	/* Turn LED off. */
 	}
 }
+
 
 /* Tarefas de exemplo que usam funcoes para suspender as tarefas por algum tempo (atraso/delay) */
 void tarefa_3(void)
@@ -155,12 +161,12 @@ semaforo_t SemaforoTeste = {0,0}; /* declaracao e inicializacao de um semaforo *
 void tarefa_5(void)
 {
 
-	uint32_t a = 0;			/* inicializaÁıes para a tarefa */
+	uint32_t a = 0;			/* inicializações para a tarefa */
 	
 	for(;;)
 	{
 		
-		a++;				/* cÛdigo exemplo da tarefa */
+		a++;				/* código exemplo da tarefa */
 
 		TarefaEspera(3); 	/* tarefa se coloca em espera por 3 marcas de tempo (ticks) */
 		
@@ -173,19 +179,19 @@ void tarefa_5(void)
 void tarefa_6(void)
 {
 	
-	uint32_t b = 0;	    /* inicializaÁıes para a tarefa */
+	uint32_t b = 0;	    /* inicializações para a tarefa */
 	
 	for(;;)
 	{
 		
-		b++; 			/* cÛdigo exemplo da tarefa */
+		b++; 			/* código exemplo da tarefa */
 		
 		SemaforoAguarda(&SemaforoTeste); /* tarefa se coloca em espera por semaforo */
 
 	}
 }
 
-/* soluÁao com buffer compartihado */
+/* soluçao com buffer compartihado */
 /* Tarefas de exemplo que usam funcoes de semaforo */
 
 #define TAM_BUFFER 10
@@ -197,7 +203,7 @@ semaforo_t SemaforoVazio = {TAM_BUFFER,0}; /* declaracao e inicializacao de um s
 void tarefa_7(void)
 {
 
-	uint8_t a = 1;			/* inicializaÁıes para a tarefa */
+	uint8_t a = 1;			/* inicializações para a tarefa */
 	uint8_t i = 0;
 	
 	for(;;)
@@ -240,8 +246,22 @@ void tarefa_8(void)
 		valor = buffer[f];
 		f = (f+1) % TAM_BUFFER;	
 		
-		(void)valor;	/* leitura da vari·vel para evitar aviso (warning) do compilador */
+		(void)valor;	/* leitura da variável para evitar aviso (warning) do compilador */
 		
 		SemaforoLibera(&SemaforoVazio);
+	}
+}
+
+/*Tarefa exemplo para indicar algum erro*/
+void tarefa_9(void){
+	volatile uint16_t c = 0;
+	for(;;)
+	{
+		c++;
+		TarefaContinua(2);
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE); 	/* Turn LED off. */
+		TarefaEspera(2000); 	/* tarefa 1 se coloca em espera por 6 marcas de tempo (ticks) */
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE); /* Liga LED. */
+		TarefaEspera(2000); 	/* tarefa 1 se coloca em espera por 6 marcas de tempo (ticks) */
 	}
 }
